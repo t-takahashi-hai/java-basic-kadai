429fa465c6239a086e0162177fd56f8fb1b15de4
t-takahashi-hai <taketakahashi@aicom.co.jp>
2025-10-16 16:28:11 +0900

    Javaの基礎を学ぼうの課題「車クラスを作って速度を出力しよう」の提出ファイルです

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,18 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		// ここで車クラスを実行
+
+		Car_Chapter15 Car1 = new Car_Chapter15();
+		
+		Car1.changeGear(3);
+		
+		Car1.run();
+		
+	}
+	
+	
+
+}
